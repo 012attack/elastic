a99b9030e5074b1ed96639c7f70a59cf135dc9b0
012attack <012attack@naver.com>
2021-11-24 15:25:31 +0900

    test

--- src/test/java/com/example/elastic/ElasticApplicationTests.java
+++ src/test/java/com/example/elastic/ElasticApplicationTests.java
@@ -1,11 +1,19 @@
 package com.example.elastic;
 
+import org.elasticsearch.action.DocWriteRequest;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.support.WriteRequest;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.core.TimeValue;
+import org.elasticsearch.index.VersionType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+import java.util.HashMap;
 
 @SpringBootTest
 class ElasticApplicationTests {
@@ -27,6 +35,38 @@ class ElasticApplicationTests {
 
     @Test
     void indexAPI(){
+        IndexRequest request = new IndexRequest("posts");
+        request.id("1");
+        String jsonString = "{" +
+                "\"user\":\"kimchy\"," +
+                "\"postDate\":\"2013-01-30\"," +
+                "\"message\":\"trying out Elasticsearch\"" +
+                "}";
+        request.source(jsonString, XContentType.JSON);
+        request.routing("routing");
+
+        request.timeout(TimeValue.timeValueSeconds(1));
+        request.timeout("1s");
+        request.setRefreshPolicy(WriteRequest.RefreshPolicy.WAIT_UNTIL);
+        request.setRefreshPolicy("wait_for");
+        request.version(2);
+        request.versionType(VersionType.EXTERNAL);
+        request.opType(DocWriteRequest.OpType.CREATE);
+        request.opType("create");
+
+
+        HashMap<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("user", "kimchy");
+        jsonMap.put("postDate", new Date());
+        jsonMap.put("message", "trying out Elasticsearch");
+        IndexRequest request2 = new IndexRequest("post2")
+                .id("1").source(jsonMap);
+
+
+
+
+
+
 
     }
 
